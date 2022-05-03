f9aaf3f486a0556201e34486e56508c9783aa318
Antonio Đurčević <antonio.durcevic@gmail.com>
2022-05-03 18:10:37 +0200

    Finished courses page homework task

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,73 +1,42 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import coursesMock from "../../lib/mock/courses";
 import Header from "../../components/Header/Header";
 import Section from "../../components/Section/Section";
 import CourseCard from "../../components/CourseCard/CourseCard";
-import LectureImg1 from "../../assets/images/lecture-1.jpg";
-import LectureImg2 from "../../assets/images/lecture-2.jpg";
-import LectureImg3 from "../../assets/images/lecture-3.jpg";
-import LectureImg4 from "../../assets/images/lecture-4.jpg";
-import LectureImg5 from "../../assets/images/lecture-5.jpg";
-import LectureImg6 from "../../assets/images/lecture-6.jpg";
-import LectureImg7 from "../../assets/images/lecture-7.jpg";
-import LectureImg8 from "../../assets/images/lecture-8.jpg";
 import { Grid, Main } from "../../lib/style/generalStyles";
 
 const Courses = () => {
+  const [courses, setCourses] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCourses(coursesMock);
+    }, 1000);
+  }, []);
+
   return (
     <>
       <Header isSecondary={[true]} />
       <Main>
         <Section Tag={"h1"} title={"All lectures"} isHeadingVisible={true}>
-          <Grid>
-            <CourseCard
-              imgSrc={LectureImg1}
-              imgAlt={"Introduction"}
-              title={"1. Introduction"}
-              subtitle={"60 Minutes"}
-            />
-            <CourseCard
-              imgSrc={LectureImg2}
-              imgAlt={"HTML & CSS"}
-              title={"2. HTML & CSS"}
-              subtitle={"120+ Minutes"}
-            />
-            <CourseCard
-              imgSrc={LectureImg3}
-              imgAlt={"Version Control System"}
-              title={"3. Version Control System"}
-              subtitle={"120+ Minutes"}
-            />
-            <CourseCard
-              imgSrc={LectureImg4}
-              imgAlt={"Advanced CSS"}
-              title={"4. Advanced CSS"}
-              subtitle={"120+ Minutes"}
-            />
-            <CourseCard
-              imgSrc={LectureImg5}
-              imgAlt={"JavaScript Basics"}
-              title={"5. JavaScript Basics"}
-              subtitle={"60 Minutes"}
-            />
-            <CourseCard
-              imgSrc={LectureImg6}
-              imgAlt={"Advanced JavaScript"}
-              title={"6. Advanced JavaScript"}
-              subtitle={"120+ Minutes"}
-            />
-            <CourseCard
-              imgSrc={LectureImg7}
-              imgAlt={"Intro to React"}
-              title={"7. Intro to React"}
-              subtitle={"120+ Minutes"}
-            />
-            <CourseCard
-              imgSrc={LectureImg8}
-              imgAlt={"React events & Conditionals"}
-              title={"8. React events & Conditionals"}
-              subtitle={"120+ Minutes"}
-            />
-          </Grid>
+          {courses && (
+            <Grid>
+              {courses.map(
+                (course, index) =>
+                  index <= courses.length && (
+                    <CourseCard
+                      key={course.id}
+                      courseId={course.id}
+                      imgSrc={course.imgSrc}
+                      imgAlt={course.imgAlt}
+                      title={course.title}
+                      subtitle={course.subtitle}
+                    />
+                  )
+              )}
+            </Grid>
+          )}
         </Section>
       </Main>
     </>
