d377a44a77ff6208cd333d5e877038b08b89a946
Antonio Đurčević <antonio.durcevic@gmail.com>
2022-06-02 18:01:01 +0200

    Changed position for HamburgerMenu

--- src/components/HamburgerMenu/HamburgerMenuStyle.js
+++ src/components/HamburgerMenu/HamburgerMenuStyle.js
@@ -14,7 +14,7 @@ export const StyledMenu = styled.nav`
   top: 0;
   right: 0;
   transition: transform 0.3s ease-in-out;
-  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+  transform: ${({ open }) => (open ? "translateY(0)" : "translateY(-100%)")};
   -webkit-backdrop-filter: blur(8px);
   backdrop-filter: blur(8px);
   z-index: 9;
